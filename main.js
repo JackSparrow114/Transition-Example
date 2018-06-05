8b8a048a78f8f484ef3f6f025ab4436d327ffc55
arpitshukla114 <arpitshukla114@yahoo.co.in>
2018-06-05 01:12:47 -0400

    Added text in the transition

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-$('.trigger').on('click', function(){
-    $(this).toggleClass('clicked');
-    document.getElementById('click-text').innerText = "Click Here!";
+$(document).ready(function(){
+    $('#trigger-text').toggle();
+    $('.trigger').on('click', function(){
+        $(this).toggleClass('clicked');
+        $('#trigger-text').toggle();
+        $('#box-text').toggle();
+    });
 });
